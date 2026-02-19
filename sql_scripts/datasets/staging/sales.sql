482c60af96c957f0484147afbc00de61fa395872
Camila Lazinski <77087519+clazinski@users.noreply.github.com>
2026-02-19 15:35:01 -0300

    fix: removed safe_cast()

--- sql_scripts/datasets/staging/sales.sql
+++ sql_scripts/datasets/staging/sales.sql
@@ -2,11 +2,11 @@ CREATE OR REPLACE TABLE `elevated-bonito-485002-k9.staging.vendas` AS
 SELECT
   id_marca,
   id_linha,
-  SAFE_CAST(data_venda AS DATE)                     AS data_venda,
-  EXTRACT(YEAR FROM SAFE_CAST(data_venda AS DATE))  AS ano_venda,
-  EXTRACT(MONTH FROM SAFE_CAST(data_venda AS DATE)) AS mes_venda,
-  LOWER(TRIM(marca))                                AS marca,
-  LOWER(TRIM(linha))                                AS linha,
-  SAFE_CAST(qtd_venda AS INT64)                     AS qtd_venda,
-  CURRENT_TIMESTAMP()                               AS data_processamento
+  CAST(data_venda AS DATE)                     AS data_venda,
+  EXTRACT(YEAR FROM CAST(data_venda AS DATE))  AS ano_venda,
+  EXTRACT(MONTH FROM CAST(data_venda AS DATE)) AS mes_venda,
+  LOWER(TRIM(marca))                           AS marca,
+  LOWER(TRIM(linha))                           AS linha,
+  CAST(qtd_venda AS INTEGER)                   AS qtd_venda,
+  CURRENT_TIMESTAMP()                          AS data_processamento
 FROM `elevated-bonito-485002-k9.raw.vendas`;
